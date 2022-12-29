82deff6efcfbd72c4adf2845b60c4269cdeca923
Trevor Karl <90585553+trevor020601@users.noreply.github.com>
2022-12-29 15:30:46 -0600

    Add files via upload

---
+++ p6_c00445623_c00441253/Server/ContactsDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using p6_c00445623_c00441253.Shared;
+namespace p6_c00445623_c00441253.Server;
+public class ContactsDbContext: DbContext
+{
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=contacts.db");
+    public DbSet<Contact>? Contacts { get; set; }
+}
